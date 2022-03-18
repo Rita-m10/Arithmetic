076cb3b10acdeb5d2aa04809eeb1ec601954bd9d
Rita-m10 <82998721+Rita-m10@users.noreply.github.com>
2022-03-18 19:39:55 +0700

    Update decoding.cpp

--- decoding.cpp
+++ decoding.cpp
@@ -53,3 +53,63 @@ int main(){
         cout << "error";
         return 0;
     }
+int x, y;
+	char sym;
+    int fullLen = 0;
+	map<char, int> Symbols;														
+	In.read((char*) & x, sizeof(x));	
+    while (x > 0){
+		In.read((char*) & sym, sizeof(sym));	
+		In.read((char*) & y, sizeof(y));	
+        fullLen += y;
+		x--;
+		Symbols[sym] = y;
+    }
+
+    list<Interval> table = CreateTab(Symbols);
+    list<Interval>::iterator it;
+    int low = 0, 
+        high = 65535, 
+        divisor = fullLen; 
+    int qtr1 = (high + 1)/4, 
+        qtr2 = qtr1*2, 
+        qtr3 = qtr1*3;
+    int value = (In.get() << 8) | In.get();
+    char c = In.get();
+    count = 0;
+    while(fullLen){
+        int freq = ((value - low + 1)*divisor - 1)/(high - low + 1);
+        for(it = table.begin(); it->right <= freq; it++);
+        int l = low;
+        low = low + (it->left)*(high - low + 1)/divisor;
+        high = l + (it->right)*(high - l + 1)/divisor - 1;
+        while(1){
+            if(high >= qtr2)
+				if(low >= qtr2){
+                low -= qtr2; 
+                high -= qtr2; 
+                value -= qtr2;
+                }else 
+					if((low >= qtr1) && (high < qtr3)){
+                        low -= qtr1; 
+                        high -= qtr1; 
+                        value -= qtr1;
+                    }else 
+						break;
+            low += low; 
+            high += high + 1;
+            value += value + (((short)c >> (7 - count)) & 1);
+            count ++;
+            if(count == 8){
+                c = In.get();
+                count = 0;
+            }
+        }
+        Out << it->sym;
+        fullLen--;
+    }
+    In.close();
+    Out.close();
+	
+    return 0;
+}
